39876e267445e1be051690f042b90bc614b57ccc
PremkumarVasudevan <prem.vasudevan@gmail.com>
2019-12-19 18:12:04 +0530

    Add files via upload

---
+++ PremJavaDemo.java
@@ -0,0 +1,9 @@
+package com.journaldev.javaprograms;
+
+import java.util.Scanner;
+
+public class PremJavaDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Introduction to openshift");
+}
